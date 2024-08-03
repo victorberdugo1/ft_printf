ad5a97b40faf1379f5cbcc40ba94c492eb4e2039
Victor Berdugo <vberdugo@car5s6.42barcelona.com>
2024-08-03 14:33:14 +0200

    1st commit

---
+++ libft/ft_putnbr_fd.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: victor <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/30 00:25:12 by victor            #+#    #+#             */
+/*   Updated: 2024/07/08 12:35:30 by vberdugo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	long	num;
+
+	num = n;
+	if (num < 0)
+	{
+		ft_putchar_fd('-', fd);
+		num = -num;
+	}
+	if (num >= 10)
+	{
+		ft_putnbr_fd((int)(num / 10), fd);
+	}
+	ft_putchar_fd(num % 10 + '0', fd);
+}
