4785a9cacb3d6ec9170a6fa9576b25a8e50a443d
Victor Berdugo <vberdugo@car7s6.42barcelona.com>
2024-08-06 15:54:04 +0200

    4rt

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,31 @@
 /*   By: vberdugo <vberdugo@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/28 14:27:23 by vberdugo          #+#    #+#             */
-/*   Updated: 2024/08/05 20:48:39 by victor           ###   ########.fr       */
+/*   Updated: 2024/08/06 15:50:19 by vberdugo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+int handle_format(const char **format, va_list args)
+{
+	int count;
+	char *str ;
+
+	count = 0;
+	(*format)++;
+	if (**format == 'c')
+		count += ft_putchar_fd((char)va_arg(args, int), 1);
+	else if (**format == 'd')
+		count += ft_putnbr_fd(va_arg(args, int), 1);
+	else if (**format == 's')
+	{
+		str = va_arg(args, char *);
+		ft_putstr_fd(str, 1);
+		count += ft_strlen(str);
+	}
+	return count;
+}
 
 int	ft_printf(char const *format, ...)
 {
@@ -18,26 +38,20 @@ int	ft_printf(char const *format, ...)
 	int		count;
 
 	count = 0;
-	va_start( args, format);
-	while (*format) {
-        if (*format == '%') {
-            format++;
-            if (*format == 'c') {
-                ft_putchar_fd((char)va_arg(args, int), 1);
-                count++;
-            } else if (*format == 'd') {
-                ft_putnbr_fd(va_arg(args, int), 1);
-
-            } else if (*format == 's') {
-                ft_putstr_fd(va_arg(args, char *), 1);
-
-            }
-        } else {
-            ft_putchar_fd(*format, 1);
-            count++;
-        }
-        format++;
-    }
-	va_end (args);
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			count += handle_format(&format, args);
+		}
+		else
+		{
+			ft_putchar_fd(*format, 1);
+			count++;
+		}
+		format++;
+	}
+	va_end(args);
 	return (count);
 }
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: vberdugo <vberdugo@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/15 12:20:41 by vberdugo          #+#    #+#             */
-/*   Updated: 2024/08/03 13:00:41 by vberdugo         ###   ########.fr       */
+/*   Updated: 2024/08/06 12:48:00 by vberdugo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,10 +62,10 @@ char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
-void	ft_putchar_fd(char c, int fd);
+int		ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
-void	ft_putnbr_fd(int n, int fd);
+int		ft_putnbr_fd(int n, int fd);
 //Bonus
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
@@ -77,6 +77,6 @@ void	ft_lstclear(t_list **lst, void (*del)(void*));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 //get_next_line
-char			*get_next_line(int fd);
+char	*get_next_line(int fd);
 
 #endif
