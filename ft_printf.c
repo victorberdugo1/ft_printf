0573d42c32d02cd6300b9ad5c4628321a930bf65
Victor Berdugo <vberdugo@car7s6.42barcelona.com>
2024-08-06 17:08:00 +0200

    5rt

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: vberdugo <vberdugo@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/28 14:27:23 by vberdugo          #+#    #+#             */
-/*   Updated: 2024/08/06 16:30:25 by vberdugo         ###   ########.fr       */
+/*   Updated: 2024/08/06 17:04:31 by vberdugo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,20 @@ int	handle_format(const char **format, va_list args)
 	(*format)++;
 	if (**format == 'c')
 		count += ft_putchar_fd((char)va_arg(args, int), 1);
-	else if (**format == 'd')
-		count += ft_putnbr_fd(va_arg(args, int), 1);
 	else if (**format == 's')
 	{
 		str = va_arg(args, char *);
 		ft_putstr_fd(str, 1);
 		count += ft_strlen(str);
 	}
+	else if (**format == 'p')
+		count += ft_putnbr_fd(va_arg(args, int), 1);
+	else if (**format == 'd' || **format == 'i' )
+		count += ft_putnbr_fd(va_arg(args, int), 1);
+	else if (**format == 'u')
+		count += ft_putnbr_fd(va_arg(args, int), 1);
+	else if (**format == 'x' || **format == 'X' )
+		count += ft_putnbr_fd(va_arg(args, int), 1);
 	return (count);
 }
 
