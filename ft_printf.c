ccbce4f1cce5841078d5c13c3cd8f8821a01555d
Victor Berdugo <vberdugo@car7s6.42barcelona.com>
2024-08-06 18:05:42 +0200

    6rt

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,62 @@
 /*   By: vberdugo <vberdugo@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/28 14:27:23 by vberdugo          #+#    #+#             */
-/*   Updated: 2024/08/06 17:04:31 by vberdugo         ###   ########.fr       */
+/*   Updated: 2024/08/06 18:03:52 by vberdugo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <unistd.h>
+
+int	ft_putnum_hex(unsigned long num)
+{
+	char	*hex_digits;
+	char	buffer[16];
+	int		i;
+	int		count;
+
+	hex_digits = "0123456789abcdef";
+	i = 0;
+	count = 0;
+	if (num == 0)
+	{
+		write(1, "0", 1);
+		return (1);
+	}
+	while (num != 0)
+	{
+		buffer[i++] = hex_digits[num % 16];
+		num /= 16;
+	}
+	while (i--)
+	{
+		write(1, &buffer[i], 1);
+		count++;
+	}
+	return (count);
+}
+
+int	ft_print_ptr(void *p)
+{
+	unsigned long	addr;
+	int				count;
+
+	count = 0;
+	addr = (unsigned long)p;
+	if (addr == 0)
+	{
+		ft_putstr_fd("(nil)", 1);
+		count += 5;
+		return (count);
+	}
+	else
+	{
+		write(1, "0x", 2);
+		count += 2;
+		count += ft_putnum_hex(addr);
+	}
+	return (count);
+}
 
 int	handle_format(const char **format, va_list args)
 {
@@ -28,7 +79,7 @@ int	handle_format(const char **format, va_list args)
 		count += ft_strlen(str);
 	}
 	else if (**format == 'p')
-		count += ft_putnbr_fd(va_arg(args, int), 1);
+		count += ft_print_ptr(va_arg(args, void *));
 	else if (**format == 'd' || **format == 'i' )
 		count += ft_putnbr_fd(va_arg(args, int), 1);
 	else if (**format == 'u')
